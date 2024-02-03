444345d77bfe3230f7a5a065073257455349fd83
Jan <jan@atrax.de>
2024-02-03 09:14:35 +0100

    Initial commit

---
+++ Source/Identity.cs
@@ -0,0 +1,17 @@
+﻿using ICities;
+
+namespace EducationalSkylinesMod
+{
+    public class Identity : IUserMod
+    {
+        public string Name
+        {
+            get { return "Educational Skylines"; }
+        }
+
+        public string Description
+        {
+            get { return "Turns Cities: Skylines into an educational Mod"; }
+        }
+    }
+}
